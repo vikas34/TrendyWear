cdfb0ad6f65be5dbf60a671c62af1522efc53c80
Vikas <vky2334@gmail.com>
2025-08-29 15:33:48 +0530

    Create Pages and their Routes

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Collection = () => {
+  return (
+    <div>
+      Collection
+    </div>
+  )
+}
+
+export default Collection
+Collection
---
+++ frontend/src/pages/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+        PlaceOrder
+        
+    </div>
+  )
+}
+
+export default PlaceOrder
