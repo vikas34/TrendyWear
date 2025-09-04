b9a662333c828a4a2b76553585b279fb69e0a3ef
Vikas <vky2334@gmail.com>
2025-09-04 16:11:54 +0530

    Completed LogIn & SignUp page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -43,11 +43,11 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <img
+          <Link to='/login'><img
             src={assets.profile_icon}
             alt="profile"
             className="w-5 cursor-pointer"
-          />
+          /></Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col w-36 gap-2 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black"> My Profile</p>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Login");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
 
-export default Login
-Login
+  };
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mt-10">
+        <p className="prata-regular text-2xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          className="w-full px-3 py-2 border border-gray-800 rounded"
+          type="text"
+          name=""
+          id=""
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 rounded"
+        type="email"
+        name=""
+        id=""
+        placeholder="Email"
+        required
+      />
+      <input
+        className="w-full px-3 py-2 border border-gray-800 rounded"
+        type="password"
+        name=""
+        id=""
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px] ">
+        <p>Forgot Your Password ? </p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4 rounded cursor-pointer">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
+
+export default Login;
+Login;
